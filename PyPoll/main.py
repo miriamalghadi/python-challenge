2c3a195e41a7622436dfc764257cce18183914ac
Miriam <miriamalghadi@gmail.com>
2020-08-22 23:20:51 -0500

    lolhelpmefinishthisb412thnxc

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,13 @@ import csv
 
 list = os.listdir("./raw_data")
 number_files = len(list)
+
+for numbers in range(number_files):
+    electioncsv = os.path.join('raw_data',"election_data_" + str(numbers+1) + ".csv")
+    
+    County= []
+    Candidate = []
+    CandidateUnique =[]
+    CVoteCount = []
+    CVotePercent =[]
+    TotalCount = 0
