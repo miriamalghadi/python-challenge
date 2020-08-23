7d32bd16ef53560320e16e601644f27f57cb5c46
Miriam <miriamalghadi@gmail.com>
2020-08-22 23:05:50 -0500

    commitxxx

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,22 @@ import csv
 
 list = os.listdir("./raw_data")
 number_files = len(list)
+
+for numbers in range(number_files):
+    budgetcsv = os.path.join('raw_data', "budget_data_" = str(numbers+1) + ".csv")
+    date = []
+    revenue = []
+    month = []
+    year = []
+    revenuechange = []
+    totalrev = 0
+    totalrevchange = 0
+    revbeg = 0
+    itemcount = 0
+
+    with open(budgetcsv, 'r') as csvFile ;
+        csvreader = csv.reader(csvfile, delimiter = ',')
+        next(csvreader, None)
+
+        for row in csvreader:
+            
